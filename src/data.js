bf13648e8bf442f68a0cc8b8de3090d842484c31
Lucas Sato <satoajato@gmail.com>
2024-04-09 17:32:27 -0300

    portfolio react commit

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export const projects = [
     description:
       "Aplicativo para auxiliar pessoas em momentos críticos, contendo recursos como: Menu para discagens emergênciais; Instruções de como lidar com uma crise; Menu contendo informações importantes.",
     image: "./vaviProject.png",
-    link: "https://www.linkedin.com/in/lucas-sato-231689197/details/projects/?profileUrn=urn%3Ali%3Afsd_profile%3AACoAAC4yPgUB2lqDfmCXPAnQoazRDWO5jnQYA9k",
+    link: "https://lnkd.in/dpNVj_CH",
   },
 ];
 
